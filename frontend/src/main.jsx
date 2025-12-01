dffce8e5c55a67f691801f6db1ef807ca0208f56
Abdirasak Mohamed Osman <Abdirasakm0@gmail.com>
2025-12-01 17:37:40 -0300

    updated frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter basename="/frameworkvs/">
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>
